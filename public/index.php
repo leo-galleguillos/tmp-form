8fdf5a8f276af6c95560157ebf5b5cc852d5feed
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-14 00:10:21 +0000

    Apply css and tweak HTML

--- public/index.php
+++ public/index.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Page</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <h1>Form Example</h1>
 
     <form action="post.php" method="post" enctype="multipart/form-data">
         <p>
-            <label for="username">Username:</label><br>
+            <label for="username">Username</label><br>
             <input type="text" name="username" id="username" required>
         </p>
         <p>
-            <label for="email">Email Address:</label><br>
+            <label for="email">Email Address</label><br>
             <input type="email" name="email" id="email" required>
         </p>
         <p>
-            <label for="image">Image Upload:</label><br>
+            <label for="image">Image Upload</label><br>
             <input type="file" name="image" id="image" accept="image/*" required>
         </p>
-        <button type="submit">Submit</button>
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
