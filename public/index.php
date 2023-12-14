60de15919e77b42b43b3559a19b029c4b0db65c3
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-14 00:25:45 +0000

    Start session in php, add semantic version number to css

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Page</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?v=0.0.0">
 </head>
 <body>
     <h1>Form Example</h1>
@@ -26,3 +29,5 @@
     </form>
 </body>
 </html>
+<?php
+unset($_SESSION['flash']);
