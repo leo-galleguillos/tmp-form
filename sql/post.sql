c156132e19e2aac749b7969d0a5c67e835cc643e
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-14 16:48:14 +0000

    Update `image` to default to NULL

--- sql/post.sql
+++ sql/post.sql
@@ -2,6 +2,6 @@ CREATE TABLE `post` (
   `post_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
