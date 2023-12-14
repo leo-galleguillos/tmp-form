a130edbc4d4c583ffd1a17abccf87fb990aba3a8
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-14 00:25:12 +0000

    Create success page

---
+++ public/success.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success</title>
+    <link rel="stylesheet" href="/css/style.css?v=0.0.0">
+</head>
+<body>
+    <h1>Success</h1>
+
+    <p>Thank you for submitting the form. We will review your form submission shortly.</p>
+</body>
+</html>
